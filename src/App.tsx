67c625a3f9a9526cb2012e8eb6358ca0bb6c9008
John Ayling <johnayling@yahoo.com>
2023-10-21 09:54:17 -0500

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+import { GameState } from "./logic.ts"
+import "./App.css"
+
+function App() {
+  const [game, setGame] = useState<GameState>()
+  useEffect(() => {
+    Rune.initClient({
+      onChange: ({ game }) => {
+        setGame(game)
+      },
+    })
+  }, [])
+
+  if (!game) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <>
+      Game
+    </>
+  )
+}
+
+export default App
