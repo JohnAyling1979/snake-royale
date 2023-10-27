5cddc2d7546cee873b001eaa1a7c94aacecc1270
John Ayling <johnayling@yahoo.com>
2023-10-27 17:40:30 -0500

    Add upgrade sound.

--- src/components/button-section/ButtonSection.tsx
+++ src/components/button-section/ButtonSection.tsx
@@ -1,11 +1,14 @@
 import { MAX_SPEED } from '../../constants';
 import Button from '../button/Button';
+import upgradePath from '../../assets/upgrade.wav';
 import styles from './ButtonSection.module.css';
 
 type ButtonSectionProps = {
   canUpgrade: boolean;
   currentSpeed: number;
 };
+
+const upgrade = new Audio(upgradePath);
 
 function ButtonSection({canUpgrade, currentSpeed}: ButtonSectionProps) {
   return (
@@ -17,8 +20,26 @@ function ButtonSection({canUpgrade, currentSpeed}: ButtonSectionProps) {
         <Button className={styles.down} text="v" onClick={() => Rune.actions.changeDirection('down')} />
       </div>
       <div className={styles.upgrades}>
-        <Button text="Speed" onClick={() => Rune.actions.increaseSpeed()} disabled={!canUpgrade || currentSpeed >= MAX_SPEED}/>
-        <Button text="Length" onClick={() => Rune.actions.increaseLength()} disabled={!canUpgrade}/>
+        <Button
+          text="Speed"
+          onClick={
+            () => {
+              upgrade.play();
+              Rune.actions.increaseSpeed();
+            }
+          }
+          disabled={!canUpgrade || currentSpeed >= MAX_SPEED}
+        />
+        <Button
+          text="Length"
+          onClick={
+            () => {
+              upgrade.play();
+              Rune.actions.increaseLength();
+            }
+          }
+          disabled={!canUpgrade}
+        />
       </div>
     </div>
   );
